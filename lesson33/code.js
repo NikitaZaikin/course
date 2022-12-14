15a11a3cc0949ac863b114715d88498577a89eac
NikitaZaikin <a.kosmin2014@gmail.com>
2022-12-14 22:57:52 +0200

    first commit in course

---
+++ lesson33/code.js
@@ -0,0 +1,32 @@
+// const arr = [ 1, 2, 3, 6, 8];
+
+// arr.pop();    // удалить последний элемент
+
+// console.log(arr);
+
+
+const arr = [ 1, 2, 3, 6, 8];
+
+// arr[99] = 0;                   //нарушение правил
+// console.log(arr.length);
+
+// console.log(arr.length);  //Могут спросить как соотносяться length и порядковый номер в массиве
+//length свойство состоит из последнего порядкового номера(индекса) и +1
+
+// arr.push(10);     // добавить последний элемент
+
+// console.log(arr);
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for ( let value of arr) {
+//     console.log(value);
+// }
+
+
+arr.forEach(function(item) {
+
+});
