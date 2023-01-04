a64cf7303e22cb4c4772fa92ae8fe071a6dd10b1
NikitaZaikin <a.kosmin2014@gmail.com>
2023-01-04 22:52:39 +0200

    finished lesson46

--- lesson46/code.js
+++ lesson46/code.js
@@ -1,9 +1,31 @@
 // console.log(document.documentElement);  Получить html
 // console.log(document.head);  Получить header
-// console.log(document.body.childNodes);
+// console.log(document.body.childNodes); для этого метода нету аналога с element
 // console.log(document.body.firstNodes);
 // console.log(document.body.lastNodes);
+
+// console.log(document.body.firstElementChild);
 
 
 // console.log(document.querySelector('#current').parentNode);   <div class= "first">...</div>
 // console.log(document.querySelector('#current').parentNode.parentNode); <div class= "wrapper">...</div>
+// console.log(document.querySelector('#current').parentElement);  <div class= "first">...</div>
+
+
+// console.log(document.querySelector('[data-current = "3"]').nextSibling);  // text он покажет перенос строки
+// console.log(document.querySelector('[data-current = "3"]').previousSibling);  // text он покажет перенос строки
+
+// console.log(document.querySelector('[data-current = "3"]').nextElementSibling); Получим объект элемента <li>4</li>
+// console.log(document.querySelector('[data-current = "3"]').previousElementSibling); Получим объект элемента <li>2</li>
+
+
+// console.log(document.body.childNodes); для того чтоб получить элементы, нужно
+
+for(let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+                                                    // Result will be <div class="wrapper">...</div>
+                                                    //                <script src="code.js"></script>
+    console.log(node);
+}
